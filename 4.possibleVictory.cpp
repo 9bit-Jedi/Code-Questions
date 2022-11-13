53b181c10e5cb345925464d529fa3ce97895c726
Utsah470 <107045013+Utsah470@users.noreply.github.com>
2022-11-13 11:09:33 +0530

    Update 4.possibleVictory.cpp

--- 4.possibleVictory.cpp
+++ 4.possibleVictory.cpp
@@ -1,9 +1,13 @@
+/*
+	https://www.codechef.com/problems/T20MCH
+	codechef : can chef win T20 game
+*/
+
 #include <iostream>
 using namespace std;
 
 int main() {
     int r, c, o;
-    
     cin >> r >> o >> c;
     
     if( (r-c) < ( (20-o)*36) ){
